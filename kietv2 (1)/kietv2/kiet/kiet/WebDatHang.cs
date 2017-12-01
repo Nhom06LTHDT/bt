e4b1b93a09989464983d1c92fb07f9d99e27da9b
BlueVT1437 <kiettranvt1709@gmail.com>
2017-12-01 13:26:03 +0700

    kietdoan
    Them Phan Gioi Thieu

---
+++ kietv2 (1)/kietv2/kiet/kiet/WebDatHang.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kiet
+{
+    public partial class WebDatHang : Form
+    {
+        public WebDatHang()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
